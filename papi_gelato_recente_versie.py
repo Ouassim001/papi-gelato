3058b98d15afed7dedd9d29cfc5fcb8d0a21c523
Ouassim001 <89780864+Ouassim001@users.noreply.github.com>
2021-11-15 09:53:20 +0100

    finished code

---
+++ papi_gelato_recente_versie.py
@@ -0,0 +1,499 @@
+bonh1 = """---------[papi-gelato]---------
+            bolletjes   1 x 0.95 = €0.95
+            hoorntjes   1 x 1.25 = €1.25
+            bakje       0 x 0.75 = €0.00
+            toppings    0 x 1.10 = €0.00                     --------+              
+            totaal                 €2.35"""
+
+bonhslagroom = """---------[papi-gelato]---------
+            bolletjes   1 x 1.10 = €1.10
+            hoorntjes   1 x 1.25 = €1.25
+            bakje       0 x 0.75 = €0.00
+            toppings    1 x 0.50 = €0.50                     --------+              
+            totaal                 €2.85"""
+
+bonhsprinkels = """---------[papi-gelato]---------
+            bolletjes   1 x 1.10 = €1.10
+            hoorntjes   1 x 1.25 = €1.25
+            bakje       0 x 0.75 = €0.00
+            toppings    1 x 0.30 = €0.30                     --------+              
+            totaal                 €2.65"""
+
+
+bonhcaramel = """---------[papi-gelato]---------
+            bolletjes   1 x 1.10 = €1.10
+            hoorntjes   1 x 1.25 = €1.25
+            bakje       0 x 0.75 = €0.00
+            toppings    1 x 0.60 = €0.60                     --------+              
+            totaal                 €2.65"""
+
+bonh2 = """---------[papi-gelato]---------
+            bolletjes   2 x 1.10 = €2.20
+            hoorntjes   1 x 1.25 = €1.25
+            bakje       0 x 0.75 = €0.00
+            toppings    0 x 1.10 = €0.00                     --------+
+            totaal                 €3.45"""
+
+bonh2slagroom = """---------[papi-gelato]---------
+            bolletjes   2 x 1.10 = €2.20
+            hoorntjes   1 x 1.25 = €1.25
+            bakje       0 x 0.75 = €0.00
+            toppings    1 x 0.50 = €0.50                     --------+
+            totaal                 €3.95"""
+
+bonh2sprinkels = """---------[papi-gelato]---------
+            bolletjes   2 x 1.10 = €2.20
+            hoorntjes   1 x 1.25 = €1.25
+            bakje       0 x 0.75 = €0.00
+            toppings    1 x 0.30 = €0.30                     --------+
+            totaal                 €3.75"""
+
+bonh2caramel = """---------[papi-gelato]---------
+            bolletjes   2 x 1.10 = €2.20
+            hoorntjes   1 x 1.25 = €1.25
+            bakje       0 x 0.75 = €0.00
+            toppings    1 x 0.60 = €0.60                     --------+
+            totaal                 €4.05"""
+
+bonh3 = """---------[papi-gelato]---------
+            bolletjes   3 x 1.10 = €3.30
+            hoorntjes   1 x 1.25 = €1.25
+            bakje       0 x 0.75 = €0.00
+            toppings    0 x 1.10 = €0.00                     --------+
+            totaal                 €4.45"""
+
+bonh3slagroom = """---------[papi-gelato]---------
+            bolletjes   3 x 1.10 = €3.30
+            hoorntjes   1 x 1.25 = €1.25
+            bakje       0 x 0.75 = €0.00
+            toppings    1 x 0.50 = €0.50                     --------+
+            totaal                 €3.95"""
+
+bonh3sprinkels = """---------[papi-gelato]---------
+            bolletjes   3 x 1.10 = €3.30
+            hoorntjes   1 x 1.25 = €1.25
+            bakje       0 x 0.75 = €0.00
+            toppings    1 x 0.30 = €0.30                     --------+
+            totaal                 €3.75"""
+
+bonh3caramel = """---------[papi-gelato]---------
+            bolletjes   3 x 1.10 = €3.30
+            hoorntjes   1 x 1.25 = €1.25
+            bakje       0 x 0.75 = €0.00
+            toppings    1 x 0.60 = €0.60                     --------+
+            totaal                 €4.05"""
+
+
+bonh4 = """---------[papi-gelato]---------
+            bolletjes   4 x 1.10 = €4.40
+            hoorntjes   1 x 1.25 = €1.25
+            bakje       0 x 0.75 = €0.00
+            toppings    0 x 1.10 = €0.00                     --------+
+            totaal                 €5.45"""
+
+bonh4slagroom = """---------[papi-gelato]---------
+            bolletjes   4 x 1.10 = €4.40
+            hoorntjes   1 x 1.25 = €1.25
+            bakje       0 x 0.75 = €0.00
+            toppings    1 x 0.50 = €0.50                     --------+
+            totaal                 €3.95"""
+
+bonh4sprinkels = """---------[papi-gelato]---------
+            bolletjes   4 x 1.10 = €4.40
+            hoorntjes   1 x 1.25 = €1.25
+            bakje       0 x 0.75 = €0.00
+            toppings    1 x 0.30 = €0.30                     --------+
+            totaal                 €3.75"""
+
+bonh4caramel = """---------[papi-gelato]---------
+            bolletjes   4 x 1.10 = €4.40
+            hoorntjes   1 x 1.25 = €1.25
+            bakje       0 x 0.75 = €0.00
+            toppings    1 x 0.60 = €0.60                     --------+
+            totaal                 €4.05"""
+
+bonh5 = """---------[papi-gelato]---------
+            bolletjes   5 x 1.10 = €5.50
+            hoorntjes   1 x 1.25 = €1.25
+            bakje       0 x 0.75 = €0.00
+            toppings    0 x 1.10 = €0.00                     --------+
+            totaal                 €6.75"""
+
+bonh5slagroom = """---------[papi-gelato]---------
+            bolletjes   5 x 1.10 = €5.50
+            hoorntjes   1 x 1.25 = €1.25
+            bakje       0 x 0.75 = €0.00
+            toppings    1 x 0.50 = €0.50                     --------+
+            totaal                 €7.25"""
+
+bonh5sprinkels = """---------[papi-gelato]---------
+            bolletjes   5 x 1.10 = €5.50
+            hoorntjes   1 x 1.25 = €1.25
+            bakje       0 x 0.75 = €0.00
+            toppings    1 x 0.30 = €0.30                     --------+
+            totaal                 €7.05"""
+
+bonh5caramel = """---------[papi-gelato]---------
+            bolletjes   5 x 1.10 = €5.50
+            hoorntjes   1 x 1.25 = €1.25
+            bakje       0 x 0.75 = €0.00
+            toppings    1 x 0.60 = €0.60                     --------+
+            totaal                 €7.35"""
+
+
+bonb1 = """---------[papi-gelato]-------------
+            bolletjes   1 x 1.10 = €1.10
+            hoorntjes   0 x 1.25 = €0.00
+            bakje       1 x 0.75 = €0.75
+            toppings    0 x 1.10 = €0.00                     --------+
+            totaal                 €1.85"""
+
+bonb1slagroom = """---------[papi-gelato]---------
+            bolletjes   1 x 1.10 = €1.10
+            hoorntjes   0 x 1.25 = €0.00
+            bakje       1 x 0.75 = €0.75
+            toppings    1 x 0.50 = €0.50                     --------+              
+            totaal                 €2.35"""
+
+bonb1sprinkels = """---------[papi-gelato]---------
+            bolletjes   1 x 1.10 = €1.10
+            hoorntjes   0 x 1.25 = €0.00
+            bakje       1 x 0.75 = €0.75
+            toppings    1 x 0.50 = €0.50                     --------+              
+            totaal                 €2.35"""
+
+bonb1caramel = """---------[papi-gelato]---------
+            bolletjes   1 x 1.10 = €1.10
+            hoorntjes   0 x 1.25 = €0.00
+            bakje       1 x 0.75 = €0.75
+            toppings    1 x 0.50 = €0.50                     --------+              
+            totaal                 €2.35"""
+
+bonb2 = """---------[papi-gelato]---------
+            bolletjes   2 x 1.10 = €2.20
+            hoorntjes   0 x 1.25 = €0.00
+            bakje       1 x 0.75 = €0.75
+            toppings    0 x 1.10 = €0.00                     --------+
+            totaal                 €2.95"""
+
+bonb2slagroom = """---------[papi-gelato]---------
+            bolletjes   2 x 1.10 = €2.20
+            hoorntjes   0 x 1.25 = €0.00
+            bakje       1 x 0.75 = €0.75
+            toppings    1 x 0.50 = €0.50                     --------+              
+            totaal                 €3.45"""
+
+bonb2sprinkels = """---------[papi-gelato]---------
+            bolletjes   2 x 1.10 = €2.20
+            hoorntjes   0 x 1.25 = €0.00
+            bakje       1 x 0.75 = €0.75
+            toppings    1 x 0.50 = €0.30                     --------+              
+            totaal                 €3.25"""
+
+bonb2caramel = """---------[papi-gelato]---------
+            bolletjes   2 x 1.10 = €2.20
+            hoorntjes   0 x 1.25 = €0.00
+            bakje       1 x 0.75 = €0.75
+            toppings    1 x 0.50 = €0.90                     --------+              
+            totaal                 €3.85"""
+
+
+bonb3 = """---------[papi-gelato]---------
+            bolletjes   3 x 1.10 = €3.30
+            hoorntjes   0 x 1.25 = €0.00
+            bakje       1 x 0.75 = €0.75
+            toppings    0 x 1.10 = €0.00                     --------+
+            totaal                 €4.05"""
+
+
+bonb3slagroom = """---------[papi-gelato]---------
+            bolletjes   3 x 1.10 = €3.30
+            hoorntjes   0 x 1.25 = €0.00
+            bakje       1 x 0.75 = €0.75
+            toppings    1 x 0.50 = €0.50                     --------+              
+            totaal                 €4.55"""
+
+bonb3sprinkels = """---------[papi-gelato]---------
+            bolletjes   3 x 1.10 = €3.30
+            hoorntjes   0 x 1.25 = €0.00
+            bakje       1 x 0.75 = €0.75
+            toppings    1 x 0.50 = €0.30                     --------+              
+            totaal                 €4.35"""
+
+bonb3caramel = """---------[papi-gelato]---------
+            bolletjes   3 x 1.10 = €3.30
+            hoorntjes   0 x 1.25 = €0.00
+            bakje       1 x 0.75 = €0.75
+            toppings    1 x 0.50 = €0.90                     --------+              
+            totaal                 €4.95"""
+
+bonb4 = """---------[papi-gelato]---------
+            bolletjes   4 x 1.10 = €4.40
+            hoorntjes   0 x 1.25 = €0.00
+            bakje       1 x 0.75 = €0.75
+            toppings    0 x 1.10 = €0.00                    --------+
+            totaal                 €5.15"""
+
+bonb4slagroom = """---------[papi-gelato]---------
+            bolletjes   4 x 1.10 = €4.40
+            hoorntjes   0 x 1.25 = €0.00
+            bakje       1 x 0.75 = €0.75
+            toppings    1 x 0.50 = €0.50                     --------+              
+            totaal                 €5.65"""
+
+bonb4sprinkels = """---------[papi-gelato]---------
+            bolletjes   4 x 1.10 = €4.40
+            hoorntjes   0 x 1.25 = €0.00
+            bakje       1 x 0.75 = €0.75
+            toppings    1 x 0.50 = €0.30                     --------+              
+            totaal                 €5.45"""
+
+bonb4caramel = """---------[papi-gelato]---------
+            bolletjes   4 x 1.10 = €4.40
+            hoorntjes   0 x 1.25 = €0.00
+            bakje       1 x 0.75 = €0.75
+            toppings    1 x 0.50 = €0.90                     --------+              
+            totaal                 €5.05"""
+
+bonb5 = """---------[papi-gelato]---------
+            bolletjes   5 x 1.10 = €5.50
+            hoorntjes   0 x 1.25 = €0.00
+            bakje       1 x 0.75 = €0.75
+            toppings    0 x 1.10 = €0.00                     --------+
+            totaal                 €6.25"""
+
+bonb5slagroom = """---------[papi-gelato]---------
+            bolletjes   5 x 1.10 = €5.50
+            hoorntjes   0 x 1.25 = €0.00
+            bakje       1 x 0.75 = €0.75
+            toppings    1 x 0.50 = €0.50                     --------+              
+            totaal                 €6.75"""
+
+bonb5sprinkels = """---------[papi-gelato]---------
+            bolletjes   5 x 1.10 = €5.50
+            hoorntjes   0 x 1.25 = €0.00
+            bakje       1 x 0.75 = €0.75
+            toppings    1 x 0.50 = €0.30                     --------+              
+            totaal                 €6.55"""
+
+bonb5caramel = """---------[papi-gelato]---------
+            bolletjes   5 x 1.10 = €5.50
+            hoorntjes   0 x 1.25 = €0.00
+            bakje       1 x 0.75 = €0.75
+            toppings    1 x 0.50 = €0.90                     --------+              
+            totaal                 €6.15"""
+
+
+
+
+
+invalid_numbers = ["6", "7", "8", "9", "10"]
+invalid = "Sorry dat is geen optie die we aanbieden..."
+
+def bedrijf():
+    bedrijf = input("Bent u 1) particulier of 2) zakelijk")
+    if bedrijf == "1":
+        start(bedrijf)
+
+def start():
+    print("Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar vanille ijs is.")
+    aantalbol = int(input("hoeveel bolletjes wil je? \n"))
+    if aantalbol in invalid_numbers:
+        print("invalid")
+    return aantalbol
+
+
+def verpakking():
+    bakhoorn = input("wilt u deze " + "bolletje(s) in   a. een hoorntje OF  b. een bakje")
+    if bakhoorn == "a":
+        return "hoorn"
+    elif bakhoorn == "b":
+        return "bakje"
+    return bakhoorn
+
+
+smaakaantal = ["1", "2", "3", "4", "5", "6", "7", "8"]
+
+def smaak1():
+    smaak1 = input("hoeveel bolletjes wilt u met de smaak aardbei?\n")
+    return smaakaantal
+
+def smaak2():
+    smaak2  = input("hoeveel bolletjes wilt u met de smaak chocolade?\n")
+    return smaakaantal
+
+def smaak3():
+    smaak3 = input("hoeveel bolletjes wilt u met de smaak munt?\n")
+    return smaakaantal
+
+def smaak4():
+    smaak4 = input("hoeveel bolletjes wilt u met de smaak vanille?\n")
+    return smaakaantal
+
+
+def toppings():
+    topping = input("""wat voor topping wilt u 
+                    a. geen
+                    b. slagroom
+                    c. sprinkels
+                    d. caramel saus\n""")
+                    
+    if topping == "a":
+        bon(bon())
+    elif topping == "b":
+        bon(bon())
+    elif topping == "c":
+        bon(bon())
+    elif topping == "d":
+        bon(bon())
+    else:
+        print(invalid)
+
+
+
+def bon():
+    bon = input("""typ hier in hoeveel bolletjes je hebt, wat voor verpakking je hebt en wat voor topping je hebt gekozen
+                    typ hier je antwoord net zoals het volgende
+                    bijvoorbeeld 3 en bakje en slagroom  of 2 en hoorntje en caramelsaus 
+                    als je niks hebt besteld, typ dan 0\n""")
+    if bon == "0":
+        start()
+    elif bon == "1 en hoorntje en geen":
+        print(bonh1)
+    elif bon == "1 en hoorntje en slagroom":
+        print(bonhslagroom)
+    elif bon == "1 en hoorntje en sprinkels":
+        print(bonhsprinkels)
+    elif bon == "1 en hoorntje en caramelsaus":
+        print(bonhcaramel)
+
+    elif bon == "2 en hoorntje en geen":
+        print(bonh2)
+    
+    elif bon == "2 en hoorntje en slagroom":
+        print(bonh2slagroom)
+    
+    elif bon == "2 en hoorntje en sprinkels":
+        print(bonh2sprinkels)
+
+    elif bon == "2 en hoorntje en caramelsaus":
+        print(bonh2caramel)
+
+    elif bon == "3 en hoorntje en geen":
+        print(bonh3)
+    elif bon == "3 en hoorntje en slagroom":
+        print(bonh3slagroom)
+
+    elif bon == "3 en hoorntje en sprinkels":
+        print(bonh3sprinkels)
+    
+    elif bon == "3 en hoorntje en caramelsaus":
+        print(bonh3caramel)
+
+    elif bon == "4 en hoorntje":
+        print(bonh4)
+    
+    elif bon == "4 en hoorntje en slagroom":
+        print(bonh4slagroom)
+    
+    elif bon == "4 en hoorntje en sprinkels":
+        print(bonh4sprinkels)
+
+    elif bon == "4 en hoorntje en caramelsaus":
+        print(bonh4caramel)
+        
+    elif bon == "5 en hoorntje":
+        print(bonh5)
+
+    elif bon == "5 en hoorntje en slagroom":
+        print(bonh5slagroom)
+
+    elif bon == "5 en hoorntje en sprinkels":
+        print(bonh5sprinkels)
+    
+    elif bon == "5 en hoorntje en caramelsaus":
+        print(bonh5caramel)
+
+    elif bon == "1 en bakje":
+        print(bonb1)
+
+    elif bon == "1 en bakje en slagroom":
+        print(bonb1slagroom)
+
+    elif bon == "1 en bakje en sprinkels":
+        print(bonb1sprinkels)
+
+    elif bon == "1 en bakje en caramelsaus":
+        print(bonb1caramel)
+
+    elif bon == "2 en bakje":
+        print(bonb2)
+
+    elif bon == "2 en bakje en slagroom":
+        print(bonb2slagroom)
+
+    elif bon == "2 en bakje en sprinkels":
+        print(bonb2sprinkels)
+
+    elif bon == "2 en bakje en caramelsaus":
+        print(bonb2caramel)
+
+    elif bon == "3 en bakje":
+        print(bonb3)
+
+    elif bon == "3 en bakje en slagroom":
+        print(bonb3slagroom)
+
+    elif bon == "3 en bakje en sprinkels":
+        print(bonb3sprinkels)
+
+    elif bon == "3 en bakje en caramelsaus":
+        print(bonb3caramel)
+
+    elif bon == "4 en bakje":
+        print(bonb4)
+    
+    elif bon == "4 en bakje en slagroom":
+        print(bonb4slagroom)
+
+    elif bon == "4 en bakje en sprinkels":
+        print(bonb4sprinkels)
+
+    elif bon == "4 en bakje en caramel":
+        print(bonb4caramel)
+
+    elif bon == "5 en bakje":
+        print(bonb5)
+
+    elif bon == "5 en bakje en slagroom":
+        print(bonb5slagroom)
+
+    elif bon == "5 en bakje en sprinkels":
+        print(bonb5sprinkels)
+
+    elif bon == "5 en bakje en caramelsaus":
+        print(bonb5caramel)
+
+    else:
+        print(invalid)
+
+        
+
+        
+def einde():
+    einde = print("hier is uw hoorntje/bakje" + "met " +  " uw bolletjes" )
+    nogmeer = input("wil je nog meer bestellen. ja/nee\n")
+    return
+    
+
+
+bedrijf()
+start()
+verpakking()
+smaak1()
+smaak2()
+smaak3()
+smaak4()
+toppings()
+bon()
+einde()
